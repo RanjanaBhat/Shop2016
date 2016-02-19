0b29e9045e0c840e0b3f549b81c64cb59246c30f
Milind Ninu Phirake <milind.phirake@anchanto.com>
2016-02-19 11:45:54 +0530

    Update orderdetail.rb

--- app/models/orderdetail.rb
+++ app/models/orderdetail.rb
@@ -6,29 +6,23 @@ class Orderdetail < ActiveRecord::Base
 	accepts_nested_attributes_for :line_items
 	has_one :address_detail
 	has_many :products, :dependent => :destroy
-	accepts_nested_attributes_for :products
-	accepts_nested_attributes_for :address_detail
-	do_not_validate_attachment_file_type :invoice
-	has_attached_file :invoice
 	has_permalink :carrier
 	def to_param
   		permalink
   	end
-  	#def customer_attributes=(attrs)
-    	#self.customer = Customer.where(attrs).first_or_initialize(attrs) 
-    	#@show_exists_message = !@customer.new_record?
-  	#end
-  	#def subtotals
-  		#items.map do 
-  			#|i| i.subtotal 
-  		#end
-	#end
-	#def total
-  		#subtotals.sum
-	#end
+  	
   	
   	#def email
   	#	Orderdetail.try(:email)
   	#end
   	#delegate :name, to: :product, prefix: true
+  	
+  	#Addedd the comments.
+  	accepts_nested_attributes_for :products
+	accepts_nested_attributes_for :address_detail
+	do_not_validate_attachment_file_type :invoice
+	has_attached_file :invoice
+	
+  	
+  	
 end
